e4db4fceaa1226fa9acd63496c1bfffd77a260e8
SadRus <rystixx@yandex.ru>
2022-11-29 23:38:53 +0300

    update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_vacancies_hh(search_text, page=0):
     return response.json()
 
 def predict_rub_salary_hh(vacancy):
-    if not vacancy['salary']['currency'] == 'RUR':return
+    if not vacancy['salary']['currency'] == 'RUR': return
     return predict_salary(vacancy['salary']['from'], vacancy['salary']['to'])
 
 def get_vacancies_statistic_hh(languages):
@@ -32,15 +32,13 @@ def get_vacancies_statistic_hh(languages):
         salaries = []
         for page in count():
             vacancies = get_vacancies_hh(language, page)
-            pages_number = vacancies['pages']
+            total_pages = vacancies['pages']
             for vacancy in vacancies['items']:
                 salary = predict_rub_salary_hh(vacancy)
                 if salary:
                     salaries.append(salary)
-            if page == pages_number:
+            if page == total_pages:
                 break
-            page += 1
-
         if salaries:
             average_salary = int(sum(salaries) / len(salaries))
         else:
@@ -77,12 +75,12 @@ def get_vacancies_statisctic_sj(languages, secret_key_sj):
     for language in languages:
         salaries = []
         for page in count():
-            vacancies_at_page = get_vacancies_sj(language, secret_key_sj, page)
-            for vacancy in vacancies_at_page['objects']:
+            vacancies = get_vacancies_sj(language, secret_key_sj, page)
+            for vacancy in vacancies['objects']:
                 salary = predict_rub_salary_sj(vacancy)
                 if salary:
                     salaries.append(salary)
-            pages_more = vacancies_at_page['more']
+            pages_more = vacancies['more']
             if not pages_more:
                 break
         if salaries:
@@ -90,7 +88,7 @@ def get_vacancies_statisctic_sj(languages, secret_key_sj):
         else:
             average_salary = None
         vacancies_statistic[language] = {
-            'vacancies_found': vacancies_at_page['total'],
+            'vacancies_found': vacancies['total'],
             'vacancies_processed': len(salaries),
             'average_salary': average_salary
             }
@@ -127,7 +125,9 @@ if __name__ == '__main__':
     languages = ['Python', 'Java', 'Javascript', 'C#', 'Go', 'C++']
 
     vacancies_statistic_hh = get_vacancies_statistic_hh(languages)
-    print(get_vacancies_table('HeadHunter Moscow', vacancies_statistic_hh, languages))
+    print(
+        get_vacancies_table('HeadHunter Moscow', vacancies_statistic_hh, languages)
+        )
 
     vacancies_statistic_sj = get_vacancies_statisctic_sj(languages, secret_key_sj)
     print(get_vacancies_table('SuperJob Moscow', vacancies_statistic_sj, languages))
